d3cea437b82c4d6205be057d9ed3bf0195828e5b
VikhashT25 <vikhasht2004@gmail.com>
2025-09-09 13:41:44 +0530

    updated

---
+++ function e() {.js
@@ -0,0 +1,5 @@
+function e() {
+    var n = "Janardhan"; 
+    console.log(n); 
+}
+e();
---
+++ hj.js
@@ -0,0 +1,10 @@
+class c{
+    g(){
+    let a=5;
+    console.log(a);
+    }
+}
+
+
+let g=new c();
+g.g();
